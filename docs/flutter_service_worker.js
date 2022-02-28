e922785f367d05d96a2a9a553a258973ec57d870
Yaroslav Kapustin <doctor.doctor.whu@gmail.com>
2022-02-28 13:13:20 +0300

    Github version updated

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -18,8 +18,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "9089ef222aa95b654337bbe961994d17",
-"/": "9089ef222aa95b654337bbe961994d17",
+"index.html": "042c727412d318835b481d3771fa170a",
+"/": "042c727412d318835b481d3771fa170a",
 "main.dart.js": "314531496d872892c377e08450310aa9",
 "manifest.json": "fa6e6851ab89e99f8a6ba3a3b8b396ad",
 "version.json": "653429bbfe4ff055ca8b4314adad6512"
